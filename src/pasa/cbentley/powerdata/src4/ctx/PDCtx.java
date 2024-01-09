377ece7fecd632878e37cb677e530786f341e3d2
Charles Bentley <cbentley@mail.ru>
2024-01-09 19:13:00 +0100

    moreunit prefs

--- src/pasa/cbentley/powerdata/src4/ctx/PDCtx.java
+++ src/pasa/cbentley/powerdata/src4/ctx/PDCtx.java
@@ -7,23 +7,48 @@ import pasa.cbentley.core.src4.ctx.UCtx;
 import pasa.cbentley.core.src4.logging.Dctx;
 import pasa.cbentley.powerdata.spec.src4.IFactoryIDStruct;
 import pasa.cbentley.powerdata.spec.src4.ctx.PDCtxA;
-import pasa.cbentley.powerdata.spec.src4.engine.TechFactory;
 import pasa.cbentley.powerdata.src4.base.Morpher;
 import pasa.cbentley.powerdata.src4.base.PowerFactory;
 
 public class PDCtx extends PDCtxA {
 
+   public static final int   CTX_ID = 14;
+
    private BOPowerDataModule boPowerData;
 
- 
-   public static final int CTX_ID= 14;
-   
+   private Morpher      morpher;
+
+   private PowerFactory powerFactory;
+
+   public PDCtx(UCtx uc, BOCtx boc) {
+      super(uc, boc);
+      
+      //#debug
+      toDLog().pInit("Created", this, PDCtx.class, "PDCtx", LVL_05_FINE, true);
+
+   }
+
+   public BOModuleAbstract getBOModule() {
+      return boPowerData;
+   }
+
    public int getCtxID() {
       return CTX_ID;
    }
-   private PowerFactory      powerFactory;
 
-   private Morpher           morpher;
+   public IJavaObjectFactory getFactory(String id) {
+      if (id == IFactoryIDStruct.ID) {
+         return getPowerFactory();
+      }
+      throw new IllegalArgumentException();
+   }
+
+   public Morpher getMorpher() {
+      if (morpher == null) {
+         morpher = new Morpher(this);
+      }
+      return morpher;
+   }
 
    public PowerFactory getPowerFactory() {
       if (powerFactory == null) {
@@ -36,40 +61,11 @@ public class PDCtx extends PDCtxA {
       this.powerFactory = powerFactory;
    }
 
-   public PDCtx(UCtx uc, BOCtx boc) {
-      super(uc, boc);
-
-   }
-
-   public Morpher getMorpher() {
-      if (morpher == null) {
-         morpher = new Morpher(this);
-      }
-      return morpher;
-   }
-
-   public BOModuleAbstract getBOModule() {
-      return boPowerData;
-   }
-
-
-
-   public IJavaObjectFactory getFactory(String id) {
-      if (id == IFactoryIDStruct.ID) {
-         return getPowerFactory();
-      }
-      throw new IllegalArgumentException();
-   }
-   
    //#mdebug
    public void toString(Dctx dc) {
       dc.root(this, "PDCtx");
       toStringPrivate(dc);
       super.toString(dc.sup());
-   }
-
-   private void toStringPrivate(Dctx dc) {
-      
    }
 
    public void toString1Line(Dctx dc) {
@@ -79,7 +75,8 @@ public class PDCtx extends PDCtxA {
    }
    //#enddebug
 
+   private void toStringPrivate(Dctx dc) {
 
-   
+   }
 
 }
